60c6aad524a9e47fd9276f4998655fb08fff575a
Hanna Meriluoto <hanna.meriluoto@gmail.com>
2022-03-14 17:23:18 +0200

    Added basic frontend to rendering books.json

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import { AgGridColumn, AgGridReact } from 'ag-grid-react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+
 import './App.css';
+import 'ag-grid-community/dist/styles/ag-grid.css'
+import 'ag-grid-community/dist/styles/ag-theme-material.css';
 
 function App() {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    fetchBooks();
+  }, [])
+
+  const fetchBooks = () => {
+    fetch('https://bookstore-800ks-default-rtdb.firebaseio.com/books/.json')
+    .then(response => response.json())
+    .then(data => setBooks(Object.values(data)))
+    .catch(err => console.error(err))
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h5" noWrap>
+            Booksore
+          </Typography>
+        </Toolbar>
+      </AppBar>
+     <div className="ag-theme-material" style={ {height:400, width: 600, margin: 'auto'} }>
+       <AgGridReact rowData={books}>
+         <AgGridColumn sortable={true} filter={true} field='title' />
+         <AgGridColumn sortable={true} filter={true} field='author' />
+         <AgGridColumn sortable={true} filter={true} field='year' />
+         <AgGridColumn sortable={true} filter={true} field='isbn' />
+         <AgGridColumn sortable={true} filter={true} field='price' />
+       </AgGridReact>
+     </div>
     </div>
   );
 }
